2a40ddae832a44debf000fde1a24092afc738e9a
Глебов Евгений Андреевич <EGlebov@synergy.ru>
2022-10-11 17:53:59 +0300

    style(components): правка стилей и логики хэдера
    НОМЕР ЗАДАЧИ: dev-001

--- components/s_header/s_header.vue
+++ components/s_header/s_header.vue
@@ -5,7 +5,7 @@
         <div class="header__logo"></div>
       </div>
       <div class="header__menu">
-          <svg class="ham hamRotate hamR" viewBox="0 0 100 100" width="35" onclick="this.classList.toggle('active')">
+          <svg class="ham hamRotate hamR" viewBox="0 0 100 100" width="40" onclick="this.classList.toggle('active')">
               <path class="line top" d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20" />
               <path class="line middle" d="m 70,50 h -40" />
               <path class="line bottom" d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20" />
@@ -27,17 +27,40 @@ export default {
   name: 's-header',
 
   data() {
-    return {};
+    return {
+      isScrolled: false,
+      scrollTop: 0,
+    };
   },
 
   computed: {},
 
   watch: {},
 
-  methods: {},
+  methods: {
+    handleScroll() {
+      const header = this.$el;
+      const headerHeight = header.offsetHeight;
+      this.scrollTop = window.scrollY;
+      switch (true) {
+        case this.scrollTop > headerHeight:
+          this.isScrolled = true;
+          header.classList.add('_scrolled')
+          break;
+        default:
+          this.isScrolled = false;
+          header.classList.remove('_scrolled');
+          break;
+      }
+
+    },
+  },
 
   beforeDestroy() {},
 
-  mounted() {},
+  mounted() {
+    window.addEventListener('scroll', this.handleScroll);
+    this.handleScroll();
+  },
 };
 </script>
