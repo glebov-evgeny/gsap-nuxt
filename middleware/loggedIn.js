d656317c201dbab97fd43817e5134a6c5922cd83
Глебов Евгений Андреевич <EGlebov@synergy.ru>
2022-11-14 15:06:58 +0300

    feat(components): route
    НОМЕР ЗАДАЧИ: main

--- middleware/loggedIn.js
+++ middleware/loggedIn.js
@@ -1,9 +1,15 @@
-export default function isLoggedIn({ store, route, redirect }) {
-  if (store.state.token == null) {
-    if (route.path === '/') {
-      return;
-    }
-    // eslint-disable-next-line consistent-return
-    return redirect('/login');
-  }
-}
+// export default function isLoggedIn({ store, route, redirect }) {
+//   if (store.state.token == null) {
+//     if (route.path === '/') {
+//       return;
+//     }
+//     return redirect('/login');
+//   }
+// }
+
+// export default function isLoggedIn({ redirect, store }) {
+//   const isAuthenticated = store.state.token;
+//   if (!isAuthenticated) {
+//     redirect({ name: 'auth' });
+//   }
+// }
