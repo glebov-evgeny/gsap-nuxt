1ae09f97ba3c7077c6bd4a85885dcc80b9e834cb
Евгений Глебов <egoji@mail.ru>
2022-11-18 10:06:05 +0300

    9703a48

---
+++ _nuxt/static/1668755155/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgsap-nuxt\u002F_nuxt\u002Fstatic\u002F1668755155",layout:"default",error:a,state:{token:a,i18n:{routeParams:{}}},serverRendered:true,routePath:"\u002F",config:{SITE_URL:"http:\u002F\u002Flocalhost",_app:{basePath:"\u002Fgsap-nuxt\u002F",assetsPath:"\u002Fgsap-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
